9b858dc1b50431aca1169bf9ab098685c823b236
alexeyws <alexfrnotific@gmail.com>
2021-02-12 17:23:22 +0300

    fetch method removed to silent env['']

--- config/initializers/avatax.rb
+++ config/initializers/avatax.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # DO NOT MODIFY FILE
-AVATAX_HEADERS = { 'X-Avalara-Client' => ENV.fetch('AVATAX_CLIENT_ID') }.freeze
+AVATAX_HEADERS = { 'X-Avalara-Client' => ENV["AVATAX_CLIENT_ID"] }.freeze
 
 module Spree
   module Avatax
