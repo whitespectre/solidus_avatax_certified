80d2d70d2c8333ac96775dc72bff6402db55ca05
alexeyws <alexfrnotific@gmail.com>
2021-02-17 17:57:08 +0300

    ADN-874 avalara_information_tab activated + small changes to fit 'tabs' style

--- app/overrides/spree/admin/users/sidebar.rb
+++ app/overrides/spree/admin/users/sidebar.rb
@@ -1,10 +1,12 @@
-# Disable Avalara information user form
-
-# Deface::Override.new(
-#   virtual_path: 'spree/admin/users/_tabs',
-#   name: 'add avalara information link',
-#   insert_bottom: '[data-hook="admin_user_tab_options"]',
-#   text: "<li>
-#         <%= link_to t('spree.avalara_information'), avalara_information_admin_user_path(@user) %>
-#       </li>"
-# )
+Deface::Override.new(
+  virtual_path: 'spree/admin/users/_tabs',
+  name: 'add_avalara_information_tab_to_user_edit',
+  insert_bottom: 'ul.tabs',
+  text: <<-ERB
+    <% if can?(:display, @user) %>
+      <li<%== ' class="active"' if current == :avalara_information %>>
+        <%= link_to t("spree.admin.user.avalara_information"), spree.avalara_information_admin_user_path(@user) %>
+      </li>
+    <% end %>
+ERB
+)
