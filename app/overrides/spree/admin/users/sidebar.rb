859382ba17d059b22aa5eaf6796887731b5bcab0
alexeyws <alexfrnotific@gmail.com>
2021-03-12 18:58:25 +0300

    ADN-914 avalara_information tab moved after all tabs in adminpanel - id version

--- app/overrides/spree/admin/users/sidebar.rb
+++ app/overrides/spree/admin/users/sidebar.rb
@@ -1,22 +1,14 @@
 # frozen_string_literal: true
 
-tab =
-  if Adn.config.admin_payouts
-    'add_payouts_tab_to_user_edit'
-  else
-    'add_sponsorship_tab_to_user_edit'
-  end
-
 Deface::Override.new(
   virtual_path: 'spree/admin/users/_tabs',
   name: 'add_avalara_information_tab_to_user_edit',
   insert_bottom: 'ul.tabs',
-  sequence: { after: tab },
   text: <<-ERB
     <% if can?(:display, @user) %>
       <li<%== ' class="active"' if current == :avalara_information %>>
-        <%= link_to t("spree.avalara_information"), spree.avalara_information_admin_user_path(@user) %>
+        <%= link_to t("spree.avalara_information"), spree.avalara_information_admin_user_path(@user), id: "avalara-information-tab" %>
       </li>
     <% end %>
-  ERB
+ERB
 )
